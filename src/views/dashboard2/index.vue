3a6979f83752e210a6d90eb70fa1c8f77d55c071
🌩 <643727197@qq.com>
2023-08-28 17:28:33 +0800

    dashboard2

---
+++ src/views/dashboard2/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="dashboard">
+    <div class="header">
+      <div>页面一</div>
+      <div>页面二</div>
+      <div>页面三</div>
+      <div>页面四</div>
+      <div>页面五</div>
+      <div>页面六</div>
+    </div>
+    <div class="main">
+      <div class="left">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </div>
+      <div class="center"></div>
+      <div class="right"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+@import "index";
+</style>
